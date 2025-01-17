66b77a22cf2fd48e2a89648819417d3ba6221373
Eduard Vilensky <embox53@gmail.com>
2025-01-17 11:51:14 +0200

    finished project section

--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
 const projectData = [
   {
@@ -7,21 +9,53 @@ const projectData = [
     title: "Clothing suggestions Website",
     description: "project 1 description",
     image: "images/projects/weather-project.jpg",
-    tag: ["All, Web"],
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Embox99/se_project_react",
+    previewUrl: "",
   },
 ];
 
 const ProjectSection = () => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectData.filter((project) =>
+    project.tag.includes(tag)
+  );
   return (
     <div>
-      <h2>My Project</h2>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Project
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
       <div>
-        {projectData.map((project) => (
+        {filteredProjects.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
             description={project.description}
             imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
           />
         ))}
       </div>
