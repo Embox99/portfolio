080cfd5055a0a16b84b7c1a70a930a08088fec20
Eduard Vilensky <embox53@gmail.com>
2025-01-23 08:56:42 +0200

    background fixed for dark theme screens

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer z-10 border border-t-[#33353F] border-l-transparent border-r-transparent">
+    <footer className="footer z-10 border border-[#33353F] border-l-transparent border-r-transparent">
       <div className="container p-12 flex justify-between">
         <span>E.Vilensky</span>
         <p className="text-slate-600">All rights reserved.</p>
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   const [navBarOpen, setNavBarOpen] = useState(false);
   const [activeSection, setActiveSection] = useState("home");
   return (
-    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#0a0a0a] opacity-90">
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#0a0a0a] opacity-90 border-l-transparent border-r-transparent">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto  px-4 py-2">
         <Link
           href={"/"}
@@ -45,14 +45,24 @@ const Navbar = () => {
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
             {navLinks.map((link) => (
               <li key={link.path}>
-                <NavLink href={link.path} title={link.title} isActive={activeSection === link.path}
-                  onSetActive={setActiveSection} />
+                <NavLink
+                  href={link.path}
+                  title={link.title}
+                  isActive={activeSection === link.path}
+                  onSetActive={setActiveSection}
+                />
               </li>
             ))}
           </ul>
         </div>
       </div>
-      {navBarOpen ? <MenuOverlay links={navLinks} onSetActive={setActiveSection} activeSection={activeSection} /> : null}
+      {navBarOpen ? (
+        <MenuOverlay
+          links={navLinks}
+          onSetActive={setActiveSection}
+          activeSection={activeSection}
+        />
+      ) : null}
     </nav>
   );
 };
